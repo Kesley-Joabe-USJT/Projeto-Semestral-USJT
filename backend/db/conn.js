f0a6dadb3bea5134f1265d4794e3b5dad8aee4a5
joabecoelho <contatojoabe@icloud.com>
2023-05-02 18:31:54 -0300

    Subindo arquivos na tag

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/instrument')
+    await mongoose.connect('mongodb://localhost:27017/teste')
     console.log('Conectou ao Mongoose!')
 }
 
