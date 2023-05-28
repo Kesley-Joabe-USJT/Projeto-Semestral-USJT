99f8c389a57440cf44df4c21bca034c75e049f50
joabecoelho <contatojoabe@icloud.com>
2023-05-28 18:17:48 -0300

    Ajustes finais

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -11,72 +11,73 @@ const getUserByToken = require('../helpers/get-user-by-token')
 
 
 module.exports = class UserController {
-    static async register(req, res){
-       const { name, email, phone, password, confirmpassword } = req.body
-
-       //validations
-       if (!name) {
-          res.status(422).json({message: "O nome é obrigatório."})
-          return
-       }
-
-       if (!email) {
-          res.status(422).json({message: "O email é obrigatório."})
-          return
-       }
-
-       if (!phone) {
-        res.status(422).json({message: "O telefone é obrigatório."})
-        return
-     }
-
-     if (!password) {
-        res.status(422).json({message: "A senha é obrigatória."})
-        return
-     }
-
-     if (!confirmpassword) {
-        res.status(422).json({message: "A confirmação de senha é obrigatória."})
-        return
-     }
-
-     if(password !== confirmpassword){
-        res.status(400).json({message: "A senha e a confirmação da senha precisam ser iguais."})
-     }
-
-     // check if user exists
-     const userExists = await User.findOne({email: email})
-
-     if(userExists) {
+   static async register(req, res) {
+      const { name, email, phone, password, confirmpassword } = req.body;
+    
+      // validations
+      if (!name) {
+        res.status(422).json({ message: "O nome é obrigatório." });
+        return;
+      }
+    
+      if (!email) {
+        res.status(422).json({ message: "O email é obrigatório." });
+        return;
+      }
+    
+      if (!phone) {
+        res.status(422).json({ message: "O telefone é obrigatório." });
+        return;
+      }
+    
+      if (!password) {
+        res.status(422).json({ message: "A senha é obrigatória." });
+        return;
+      }
+    
+      if (!confirmpassword) {
+        res.status(422).json({ message: "A confirmação de senha é obrigatória." });
+        return;
+      }
+    
+      if (password !== confirmpassword) {
         res
-        .status(422)
-        .json({
-            message: "Email já cadastrado!"
-        })
-        return
-     }
-
-     // create password
-     const salt = await bcrypt.genSalt(12)
-     const passwordHash = await bcrypt.hash(password, salt)
-
-     // create user
-     const user = new User({
+          .status(400)
+          .json({ message: "A senha e a confirmação da senha precisam ser iguais." });
+        return;
+      }
+    
+      // check if user exists
+      const userExists = await User.findOne({ email: email });
+    
+      if (userExists) {
+        res.status(422).json({
+          message: "Email já cadastrado!"
+        });
+        return;
+      }
+    
+      // create password
+      const salt = await bcrypt.genSalt(12);
+      const passwordHash = await bcrypt.hash(password, salt);
+    
+      // create user
+      const user = new User({
         name,
         email,
         phone,
-        password: passwordHash,
-     })
-
-     try {
-        const newUser = await user.save()
-
-        await createUserToken(newUser, req, res)
-        
-     } catch (error) {
-        res.status(500).json({message: error})
-     }
+        password: passwordHash
+      });
+    
+      try {
+        const newUser = await user.save();
+    
+        await createUserToken(newUser, req, res);
+      } catch (error) {
+        res.status(500).json({ message: error });
+      }
     }
+    
 
     static async login(req, res) {
 
